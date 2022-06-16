ae25bac043f352869d770ad207937be41c4a6768
Damith Dilhan <55306956+DamithDilhan@users.noreply.github.com>
2022-06-16 10:01:18 +0530

    build: compile new index.js code

--- dist/index.js
+++ dist/index.js
@@ -8863,18 +8863,82 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(4181)
 const github = __nccwpck_require__(5163);
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
+
+const getLastPRStatus = `query($owner:String!, $repo:String!{
+    repository(owner: $owner, name: $repo) {
+      owner {
+        id
+      }
+      primaryLanguage {
+        name
+      }
+      description
+      updatedAt
+      languages(first: 100) {
+        totalSize
+        edges {
+          size
+          node {
+            name
+            color
+            id
+          }
+        }
+      }
+      pullRequests(last: 1, states: OPEN) {
+        edges {
+          node {
+            number
+            url
+            commits(last: 1) {
+              nodes {
+                commit {
+                  commitUrl
+                  oid
+                  status {
+                    contexts {
+                      context
+                      state
+                      targetUrl
+                      description
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }`
+
+async function run() {
+  try {
+
+    const myToken = core.getInput("githubToken")
+
+    const octokit = github.getOctokit(myToken)
+    // last pr check result
+    const result = await octokit.graphql({
+      query: getLastPRStatus,
+      repo: github.context.payload.repository.name,
+      owner: github.context.payload.repository.owner.name
+    })
+
+    console.log(result)
+
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
+    //const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+  } catch (error) {
     core.setFailed(error.message);
+  }
+
 }
+
+run()
 })();
 
 module.exports = __webpack_exports__;
