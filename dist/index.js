73aa8e3aa9ecd3539de080783630e34b30d52c06
Damith Dilhan <55306956+DamithDilhan@users.noreply.github.com>
2022-06-21 20:44:02 +0530

    build: build after error fix

--- dist/index.js
+++ dist/index.js
@@ -342,7 +342,7 @@ const repoDataModel = (graphql_result, open_prs = []) => {
             orgId: graphql_result.owner.id,
             repoName: graphql_result.name,
             repoUrl: graphql_result.url,
-            updateAt: graphql_result.updateAt,
+            updatedAt: graphql_result.updatedAt,
             id: graphql_result.id,
             openPRs: []
 
